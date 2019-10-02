ec455c206eb33240b71e760bb2982dc9d5f70109
cladams0203 <cladams0203@gmail.com>
2019-10-02 15:54:23 -0400

    finished part of the component routing with props

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Header } from './components/Header';
 import { Route } from 'react-router-dom';
+import { Mac } from './components/Mac';
+import { Iphone } from './components/Iphone';
+import { Ipad } from './components/Ipad';
 import './App.css';
 import macInfo from './macInfo.json';
 
@@ -11,9 +14,9 @@ function App() {
   return (
     <div className="App">
       <Route path='/' render={props => <Header {...props} appleData={appleData} /> } />
-      <Route path='/imac' />
-      <Route path='/ipad' />
-      <Route path='/iphone' />
+      <Route path='/imac' render={props => <Mac {...props} appleData={appleData} /> } />
+      <Route path='/ipad' render={props => <Ipad {...props} appleData={appleData} /> } />
+      <Route path='/iphone' render={props => <Iphone {...props} appleData={appleData} /> } />
       <Route path='/watch' />
       <Route path='/tv' />
       <Route path='/music' />
